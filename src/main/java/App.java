c2e9778b028ac062f26888316580e392cb87b40d
Vitaliy Yudin <vitalij.iud@yandex.ru>
2024-05-04 13:15:39 +0300

    Выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanSame =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanSame.getMessage());
+        System.out.println("Hello world: " + (bean == beanSame));
+        Cat firstCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(firstCat.getAge() + " " + firstCat.getName());
+        Cat secondCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(secondCat.getAge() + " " + secondCat.getName());
+        System.out.println("Cat: " + (firstCat == secondCat));
     }
 }
